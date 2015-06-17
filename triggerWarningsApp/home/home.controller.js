146731438cf8735ac2409191d11794ba2a906add
Alex Winson <awinson@neworbit.co.uk>
2015-06-17 13:49:35 +0100

    layout tweeks, twitter added

--- triggerWarningsApp/home/home.controller.js
+++ triggerWarningsApp/home/home.controller.js
@@ -1,10 +1,14 @@
 (function() {
     'use strict';
     angular.module('triggerWarningsApp.home').controller('HomeCtrl', [
-        '$scope', 'auth',
-        function($scope, auth) {
+        '$scope', 'auth', '$state',
+        function($scope, auth, $state) {
             $scope.isLoggedIn = auth.isLoggedIn();
             $scope.logOut = auth.logOut;
+
+            $scope.goHome = function(){
+            	$state.go('dashboard');
+            };
         }
     ]);
 })();
